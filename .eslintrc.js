911f39f245c9e035efa080295950620fb800d4e3
Alberto <albicento.ac@gmail.com>
2024-03-12 10:55:16 +0100

    chore: linting hook

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+      es2021: true,
+      node: true,
+      'react-native/react-native': true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:react-native/all',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+      '@typescript-eslint',
+      'react-hooks',
+      'react-native',
+    ],
+    rules: {
+      // Custom rules here. Example:
+      'react/react-in-jsx-scope': 'off', // Not needed with React 17+
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      // Adjust rules based on your project needs
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+    },
+  };
+  
--- App.tsx
+++ App.tsx
@@ -12,9 +12,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
   },
 });
