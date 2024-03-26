41332b39ca33c2606a7e02922d564935ff29f414
Fyuryy <plagouveia@gmail.com>
2024-03-26 16:41:10 +0100

    feat: added color constants

--- assets/colors/colors.ts
+++ assets/colors/colors.ts
@@ -1,2 +1,5 @@
-export const white = 'white'
-export const peach = '#FD9890'
+export const white = 'white';
+export const peach = '#FD9890';
+export const red = 'red';
+export const blue = 'blue';
+export const black = 'black';
