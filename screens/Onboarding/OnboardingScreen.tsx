e9d3b8b9ab58c0c6f48530044ea3ab002d182a8a
Aidasvenc <79483254+Aidasvenc@users.noreply.github.com>
2024-03-26 11:04:59 +0100

    refactor: add a folder for screens

---
+++ screens/Onboarding/OnboardingScreen.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native"
+import { styles } from "./styles"
+
+export const OnboardingScreen = () => {
+    <View style={styles.container}>
+        <Text>This is the onboarding screen</Text>
+    </View>
+}
