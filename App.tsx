b8d996718b07a7b1a93b426e392bf815af631120
Fyuryy <plagouveia@gmail.com>
2024-04-02 17:01:05 +0200

    feat: added SplashScreen for loading screen

--- App.tsx
+++ App.tsx
@@ -2,9 +2,8 @@
 import InformationScreen from './screens/Registration/InformationScreen/InformationScreen'
 //import InterestsScreen from './screens/Registration/InterestsScreen/InterestsScreen'
 
-
-import {Text} from 'react-native';
-
+import React from 'react';
+import * as SplashScreen from 'expo-splash-screen'
 import { JetBrainsMono_100Thin_Italic,JetBrainsMono_400Regular, JetBrainsMono_700Bold, useFonts} from '@expo-google-fonts/jetbrains-mono'; 
 
 
@@ -20,8 +19,8 @@ export default function App() {
   if(!fontsLoaded){
     //loading screen
     return (
-    <Text>Loading...</Text>);
-  }
+      SplashScreen.preventAutoHideAsync()
+    )}
 
 
   return (
