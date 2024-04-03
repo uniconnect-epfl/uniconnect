6392e3405869006bb028f0eda05681be8f3fb30c
Fyuryy <plagouveia@gmail.com>
2024-04-03 17:48:53 +0200

    refactor: changed name

--- assets/global/globalStyles.ts
+++ assets/global/globalStyles.ts
@@ -1,12 +1,16 @@
 // styles/GlobalStyles.ts
 import { StyleSheet } from 'react-native';
 
-export const GlobalStyles = StyleSheet.create({
-    boldText: {
-      fontFamily: 'JetBrainsMono_700Bold',
-    },
+
+
+export const globalStyles = StyleSheet.create({
+  boldText: {
+    fontFamily: 'JetBrainsMono_700Bold',
+  },
   text: {
     fontFamily: 'JetBrainsMono_400Regular',
     // Add other common properties
   },
 });
+
+
