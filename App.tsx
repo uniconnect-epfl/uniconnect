ec25ceeab9ec0376fe27e6f77788d89b698df2b4
Fyuryy <plagouveia@gmail.com>
2024-04-03 17:50:04 +0200

    feat: added safe-area-context library

--- App.tsx
+++ App.tsx
@@ -2,9 +2,13 @@ import React, { useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 //import OnboardingScreen from './screens/Onboarding/OnboardingScreen';
 import InformationScreen from './screens/Registration/InformationScreen/InformationScreen';
+//import InterestsScreen from './screens/Registration/InterestsScreen/InterestsScreen';
 import { useFonts, JetBrainsMono_100Thin_Italic, JetBrainsMono_400Regular, JetBrainsMono_700Bold } from '@expo-google-fonts/jetbrains-mono';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
 
 export default function App() {
+
   const [fontsLoaded] = useFonts({ 
     JetBrainsMono_100Thin_Italic, 
     JetBrainsMono_400Regular, 
@@ -32,7 +36,9 @@ export default function App() {
   }
 
   return (
-  <InformationScreen/>)
-  //<OnboardingScreen />;
-  
+    <SafeAreaProvider>
+      <InformationScreen/>  
+      {/* <InterestsScreen/>   */}
+      {/* <OnboardingScreen/>  */}
+    </SafeAreaProvider>) 
 }
