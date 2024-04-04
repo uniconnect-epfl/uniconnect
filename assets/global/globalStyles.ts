fb169b9ab3504093c0427ffd1a4f125809ac2a68
Fyuryy <plagouveia@gmail.com>
2024-04-04 13:09:59 +0200

    feat: added AuthenticationScreen
    (not 100% finished, but better to commit now for merge purposes)

--- assets/global/globalStyles.ts
+++ assets/global/globalStyles.ts
@@ -1,16 +1,13 @@
-// styles/GlobalStyles.ts
 import { StyleSheet } from 'react-native';
-
-
 
 export const globalStyles = StyleSheet.create({
   boldText: {
     fontFamily: 'JetBrainsMono_700Bold',
   },
+  smallText: {
+    fontSize: 10,
+  },
   text: {
     fontFamily: 'JetBrainsMono_400Regular',
-    // Add other common properties
   },
 });
-
-
---
+++ screens/Registration/AuthenticationScreen/AuthenticationScreen.tsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+import InputField from '../../../components/InputField/InputField';
+
+import styles from './styles';
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { globalStyles } from '../../../assets/global/globalStyles';
+import Divider from '../../../components/divider/divider';
+import LowBar from '../../../components/LowBar/LowBar';
+import { Entypo } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
+const Form: React.FC = () => {
+  const insets = useSafeAreaInsets();
+  const MIN_LENGHT = 8;
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [confirmEmail, setConfirmEmail] = useState('');
+
+  const isPassword = () => {
+    return password.length >= MIN_LENGHT;
+  };
+
+  const doPasswordsMatch = () => {
+    return password === confirmPassword;
+  };
+
+  const isEmail = () => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const doEmailsMatch = () => {
+    return email == confirmEmail;
+  };
+
+  /**
+   * Function that submits the user data to the DB
+   */
+  const submitForm = () => {
+    if (isPassword() && doPasswordsMatch() && isEmail() && doEmailsMatch()) {
+      return;
+    } else {
+      return;
+    }
+  };
+
+  return (
+    <View style={{ paddingBottom: insets.bottom, paddingTop: insets.top }}>
+      <Image
+        source={require('../../../assets/icon.png')}
+        style={styles.image}
+      />
+
+      <Text style={[styles.title, globalStyles.boldText]}>Last Step</Text>
+
+      <InputField
+        label="Password*"
+        placeholder="****************"
+        onChangeText={setPassword}
+        value={password}
+      ></InputField>
+
+      <InputField
+        label="Confirm Password*"
+        placeholder="****************"
+        onChangeText={setConfirmPassword}
+        value={confirmPassword}
+      ></InputField>
+
+      <View style={styles.container}>
+        <View style={styles.phrase}>
+          <Entypo name="cross" color="red" />
+          <Text style={[globalStyles.text, globalStyles.smallText]}>
+            Password strength
+          </Text>
+        </View>
+
+        <View style={styles.phrase}>
+          {!doPasswordsMatch() && <Entypo name="cross" color="red" />}
+          {doPasswordsMatch() && <AntDesign name="check" color="green" />}
+          <Text style={[globalStyles.text, globalStyles.smallText]}>
+            Passwords matching
+          </Text>
+        </View>
+
+        <View style={styles.phrase}>
+          {!isPassword() && <Entypo name="cross" color="red" />}
+          {isPassword() && <AntDesign name="check" color="green" />}
+          <Text style={[globalStyles.text, globalStyles.smallText]}>
+            At least 8 characters
+          </Text>
+        </View>
+
+        <View style={styles.phrase}>
+          <Entypo name="cross" color="red" />
+          <Text style={[globalStyles.text, globalStyles.smallText]}>
+            Contains a number and a symbol
+          </Text>
+        </View>
+      </View>
+
+      <Divider />
+
+      <InputField
+        label="E-mail*"
+        placeholder="E-mail"
+        onChangeText={setEmail}
+        value={email}
+      ></InputField>
+
+      <InputField
+        label="Confirm e-mail*"
+        placeholder="Confirm your e-mail"
+        onChangeText={setConfirmEmail}
+        value={confirmEmail}
+      ></InputField>
+
+      <TouchableOpacity style={styles.button} onPress={submitForm}>
+        <Text style={globalStyles.boldText}>Send confirmation e-mail</Text>
+      </TouchableOpacity>
+
+      <LowBar />
+    </View>
+  );
+};
+export default Form;
