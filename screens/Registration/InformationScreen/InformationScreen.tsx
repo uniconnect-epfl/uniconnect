566b84fe494c133e00091a176ea9c5df451290b0
Wepo <alex.mourot@icloud.com>
2024-04-07 15:06:19 +0200

    fix: information screen should only contains the navigator

--- screens/Registration/InformationScreen/InformationScreen.tsx
+++ screens/Registration/InformationScreen/InformationScreen.tsx
@@ -1,11 +1,9 @@
-import { View, Text } from "react-native"
+
 import HomeTabNavigator from "../../../navigation/Home/HomeTabNavigator"
 
 
 export const InformationScreen = () => {
   return (
-    <><View>
-      <Text>This is the Information screen</Text>
-    </View><HomeTabNavigator /></>
+    <><HomeTabNavigator /></>
   )
 }
