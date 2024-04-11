880adf65621a841d234cf8430c3afd0e55a64686
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-11 20:39:54 +0200

    test: launching app test

--- __test__/screens/App.test.tsx
+++ __test__/screens/App.test.tsx
@@ -1,7 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import App from '../../App'
 
-describe('<App />', () => {
-  it('dummy test', () => {
-    const value = true
-    expect(value).toBeTruthy()
+describe('App', () => {
+  
+  it('renders correctly', () => {
+    const component = render(<App />)
+    expect(component).toBeTruthy()
   })
+  
 })
