5e2536e2030cf99a2a730a66548b2a6b7f3a8dc2
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-11 20:36:59 +0200

    test: navigators tests

--- __test__/navigation/Home/HomeTabNavigator.test.tsx
+++ __test__/navigation/Home/HomeTabNavigator.test.tsx
@@ -1,6 +1,15 @@
-describe('<HomeTabNavigator />', () => {
-    it('dummy test', () => {
-      const value = true
-      expect(value).toBeTruthy()
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import HomeTabNavigator from '../../../navigation/Home/HomeTabNavigator'
+import { NavigationContainer } from '@react-navigation/native'
+
+describe('HomeTabNavigator', () => {
+    it('renders the bottom tab navigator with expected initial route', () => {
+        const component = render(
+            <NavigationContainer>
+                <HomeTabNavigator />
+            </NavigationContainer>
+        )
+        expect(component).toBeTruthy()
     })
-  })
+})
