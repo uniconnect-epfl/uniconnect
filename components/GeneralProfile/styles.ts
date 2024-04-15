ecbc9cc55694c01576e635e18eb2501d9819ba41
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-15 16:03:06 +0200

    feat: added component GeneralProfile

---
+++ components/GeneralProfile/styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native"
+import { peach } from "../../assets/colors/colors"
+
+export const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        left: 10,
+        margin: 10,
+    },
+    horizontalContainer: {
+        flexDirection: "row"
+    },
+    location: {
+        fontSize: 11,
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginVertical: 5,
+    },
+    profilePicture: {
+        alignItems: 'center',
+        backgroundColor: peach,
+        borderRadius: 40, // to make it circular
+        height: 80,
+        justifyContent: 'center',
+        width: 80,
+    },
+
+})
