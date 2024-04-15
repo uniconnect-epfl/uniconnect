cc1a61463e9aa560dca897f179d27d97c3ca6b3d
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-15 16:02:13 +0200

    feat: added component ExpandableDescription

---
+++ components/ExpandableDescription/ExpandableDescription.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { black } from '../../assets/colors/colors'
+import { styles } from "./styles"
+
+interface ExpandableDescriptionProps {
+  description: string;
+}
+
+const ExpandableDescription: React.FC<ExpandableDescriptionProps> = ({ description }) => {
+  const initialNbLines = 4
+  const [limitNbLines, setLimitNbLines] = useState(true)
+
+  return (
+    <View>
+      <Text 
+          style={styles.text}
+          numberOfLines={limitNbLines ? initialNbLines : undefined}>
+          {description}
+        </Text>
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={() => setLimitNbLines(!limitNbLines) }>
+          <Ionicons name={limitNbLines ? "chevron-down-outline" : "chevron-up-outline"} size={24} color={black} />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default ExpandableDescription
