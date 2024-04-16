55f182610f0cc1e290aea0e321ed4cb23a7e4c6f
GustaveCharles <91184289+GustaveCharles@users.noreply.github.com>
2024-04-16 10:12:14 +0200

    feat: added persistent storage

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app" 
-import { getAuth } from 'firebase/auth' 
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth' 
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,6 +15,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig) 
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP) 
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  }) 
 
 
