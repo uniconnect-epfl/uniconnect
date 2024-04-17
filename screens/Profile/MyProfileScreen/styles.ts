9042bf21fb60d35709d6e57b2a9111a58ee5351f
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-17 10:55:44 +0200

    Profile screens components (#75)
    * feat: added component ExpandableDescription
    
    * feat: added component GeneralProfile
    
    * feat: modified my profile so we can see the components
    
    * test: added unit tests for ExpandableDescription
    
    * test: added unit tests for GeneralProfile
    
    * refactor: used single quotes everywhere
    
    ---------
    
    Co-authored-by: GaelCondeLosada <107279595+Gaellosada@users.noreply.github.com>

--- screens/Profile/MyProfileScreen/styles.ts
+++ screens/Profile/MyProfileScreen/styles.ts
@@ -1,9 +1,13 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
+  leftAlign: {
+    alignSelf: 'flex-start'
+  },
   view: {
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center'
-  }
+  },
+  
 })
