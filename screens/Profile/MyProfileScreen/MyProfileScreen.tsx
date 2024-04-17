3a1e9c913a93a70109f3df4e1d612d018ec5222a
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-17 16:06:17 +0200

    feat: half functional my profile screen

--- screens/Profile/MyProfileScreen/MyProfileScreen.tsx
+++ screens/Profile/MyProfileScreen/MyProfileScreen.tsx
@@ -1,23 +1,98 @@
-import { View } from 'react-native'
-import { styles } from './styles'
+import { Text, View, TouchableOpacity } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 import ExpandableDescription from '../../../components/ExpandableDescription/ExpandableDescription'
 import GeneralProfile from '../../../components/GeneralProfile/GeneralProfile'
+import { styles } from './styles'
+import { black } from '../../../assets/colors/colors'
+import InputField from '../../../components/InputField/InputField'
+import { useState } from 'react'
+
+type Contact = {
+  uid: string
+  firstName: string
+  lastName: string
+  profilePictureUrl?: string
+  description: string
+  interests: string[]
+  qualifications: string[],
+  location: string
+}
+
+const dummyProfile1: Contact = {
+  uid: "4",
+  firstName: "Hervé",
+  lastName: "Dela",
+  profilePictureUrl: "",
+  description: "Description, description, descrc hdsjklaf hel fhj fdj bjhd vbfdhj vbdjhl vdjlh vdhj vfdjh hvdu vdh vdfj vdfs hfdj fdj vdfjl d grei hrj heiua fjispd jis gjdrkl gjreks reksl grei gril nrsj njiption, loooooooooong descriiiiiiiiiiption, this is a veeeeeeeeeeeeeeeeeeeeeery loooooooooooooooooooooooooooooooooooooooooooong descriiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiption alala",
+  interests: ["running"],
+  qualifications: ["bowling"],
+  location: "EPFL Ecublens"
+}
+
+//const dummyProfile2: Contact = {
+//  uid: "4",
+//  firstName: "Hervé",
+//  lastName: "DelaMontagnophobe",
+//  profilePictureUrl: "",
+//  description: "Description, description, description, loooooooooong descriiiiiiiiiiption, this is a veeeeeeeeeeeeeeeeeeeeeery loooooooooooooooooooooooooooooooooooooooooooong descriiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiption alala",
+//  interests: ["running"],
+//  qualifications: ["bowling"],
+//  location: "EPFL Ecublens"
+//}
+
+const dummyProfile = dummyProfile1
 
 export const MyProfileScreen = () => {
+  const [search, setSearch] = useState('')
+
   return (
-    <View style={styles.view}>
+    <View style={styles.container}>
 
-      <View style={styles.leftAlign}>
-        <GeneralProfile
-          name={'Name'}
-          surname={'Surname'}
-          location={'le Mont-sur-Lausanne'}
+      <View style={styles.topBackground} />
+
+      <View style={styles.profileContainer}>
+
+        <View style={styles.horizontalContainer}>
+
+          <View style={styles.leftAlign}>
+            <GeneralProfile
+              name={dummyProfile.firstName}
+              surname={dummyProfile.lastName}
+              location={dummyProfile.location}
+            />
+          </View>
+
+          <View style={styles.horizontalContainer}>
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Update</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button}>
+              <View style={styles.horizontalContainer}>
+                <Ionicons name='qr-code' size={14} color={black} />
+                <Text style={styles.buttonText}>QR</Text>
+              </View>
+            </TouchableOpacity>
+
+          </View>
+
+        </View>
+
+        <ExpandableDescription
+          description={dummyProfile.description}
         />
-      </View>
 
-      <ExpandableDescription
-        description={'slajfeiohadslh oh ho sdhv lsvnjls bvdfjl vj shdls hvueri hvueos hvurei shvureio vhruieo vhuris hviuds vnfjd re vuirep uir hfeuiphgfure fhueir hguriewp hfurie hfruie hfuire hfure uire uier huirs hui bhuero vureso huire hure hzureepseo guers hvuip vuip hurieps hurips hvurips bhutrid bhurtipd hbuips hurieps huiers vhurieps vuries hvureips hureis bfhres fures huirpe burip vuirso hvzureo gfuersh guirp hutirp hveruips vuresp hv'}
-      />
+        <View style={styles.separatorLine} />
+
+        <InputField
+          label=''
+          placeholder='Search...'
+          value={search}
+          onChangeText={text => setSearch(text)}>
+        </InputField>
+
+      </View>
 
     </View>
   )
--- screens/Profile/MyProfileScreen/styles.ts
+++ screens/Profile/MyProfileScreen/styles.ts
@@ -1,13 +1,58 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native"
+import { black, lightPeach, peach } from "../../../assets/colors/colors"
 
 export const styles = StyleSheet.create({
-  leftAlign: {
-    alignSelf: 'flex-start'
+  button: {
+    alignItems: "center",
+    backgroundColor: lightPeach,
+    borderColor: peach,
+    borderRadius: 10,
+    borderWidth: 2,
+    height: 38,
+    justifyContent: "center",
+    marginLeft: 14,
+    width: 90,
   },
-  view: {
-    alignItems: 'center',
+  buttonText: {
+    alignItems: "center",
+    fontSize: 12,
+    fontWeight: "bold",
+    justifyContent: "center",
+    margin: 1,
+  },
+  container: {
+    alignItems: "center",
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: "center",
+  },
+  descriptionStyle: {
+    width: "100%"
+  },
+  horizontalContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  leftAlign: {
+    alignSelf: "flex-start",
+    flex: 1,
+  },
+  profileContainer: {
+    flex: 1,
+    marginTop: -35,
+  },
+  searchBar: {
+    
+  },
+  separatorLine: {
+    backgroundColor: black,
+    height: 1,
+    width: "auto",
+  },
+  topBackground: {
+    backgroundColor: lightPeach,
+    height: 160,
+    width: "100%"
   },
   
 })
