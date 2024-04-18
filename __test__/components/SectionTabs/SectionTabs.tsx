bee1cf696a6b1e222025a32a81d9c3d9aec46162
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-18 18:40:18 +0200

    test: updated test for properties renaming

--- __test__/components/SectionTabs/SectionTabs.tsx
+++ __test__/components/SectionTabs/SectionTabs.tsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import { render, fireEvent } from '@testing-library/react-native'
-import SectionTabs from '../../../components/SectionTabs/SectionTabs'
+import React from "react"
+import { render, fireEvent } from "@testing-library/react-native"
+import SectionTabs from "../../../components/SectionTabs/SectionTabs"
 
-describe('SectionTabs', () => {
-    const choices = ['Option 1', 'Option 2', 'Option 3']
+describe("SectionTabs", () => {
+    const tabs = ["Option 1", "Option 2", "Option 3"]
 
-    it('should render correctly with the initial choice', () => {
-        const { getByText } = render(<SectionTabs choices={choices} startingChoice="Option 2" selectBarWidth={80} onChoiceChange={() => {}}/>)
-        expect(getByText('Option 2')).toBeTruthy()
+    it("should render correctly with the initial choice", () => {
+        const { getByText } = render(<SectionTabs tabs={tabs} startingTab="Option 2" barWidth={80} onTabChange={() => {}}/>)
+        expect(getByText("Option 2")).toBeTruthy()
     })
 
-    it('should call onChoiceChange when a choice is pressed', () => {
-        const onChoiceChangeMock = jest.fn()
-        const { getByText } = render(<SectionTabs choices={choices} startingChoice="Option 1" onChoiceChange={onChoiceChangeMock} selectBarWidth={80} />)
+    it("should call onTabChange when a choice is pressed", () => {
+        const onTabChangeMock = jest.fn()
+        const { getByText } = render(<SectionTabs tabs={tabs} startingTab="Option 1" onTabChange={onTabChangeMock} barWidth={80} />)
 
-        const optionToPress = getByText('Option 2')
+        const optionToPress = getByText("Option 2")
         fireEvent.press(optionToPress)
 
-        expect(onChoiceChangeMock).toHaveBeenCalledWith('Option 2')
-        expect(onChoiceChangeMock).toHaveBeenCalledTimes(1)
+        expect(onTabChangeMock).toHaveBeenCalledWith("Option 2")
+        expect(onTabChangeMock).toHaveBeenCalledTimes(1)
     })
 
-    it('should update the selected choice visually when a different choice is pressed', () => {
-        const { getByText, rerender } = render(<SectionTabs choices={choices} startingChoice="Option 1" selectBarWidth={80} onChoiceChange={() => {}} />)
+    it("should update the selected choice visually when a different choice is pressed", () => {
+        const { getByText, rerender } = render(<SectionTabs tabs={tabs} startingTab="Option 1" barWidth={80} onTabChange={() => {}} />)
 
-        const optionToPress = getByText('Option 3')
+        const optionToPress = getByText("Option 3")
         fireEvent.press(optionToPress)
 
-        rerender(<SectionTabs choices={choices} startingChoice="Option 3" selectBarWidth={80} onChoiceChange={() => {}} />)
+        rerender(<SectionTabs tabs={tabs} startingTab="Option 3" barWidth={80} onTabChange={() => {}} />)
     })
 })
