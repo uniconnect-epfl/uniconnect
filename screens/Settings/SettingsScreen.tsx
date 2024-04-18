8e6f1887a3fd3eb10ace41b6be7714e303403f00
Alberto <albicento.ac@gmail.com>
2024-04-18 23:52:21 +0200

    feat: added simple settings

--- screens/Settings/SettingsScreen.tsx
+++ screens/Settings/SettingsScreen.tsx
@@ -1,11 +1,36 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
 import { styles } from "./styles"
-
+import { useNavigation } from "@react-navigation/native"
 
 export const SettingsScreen = () => {
+  const navigation = useNavigation()
+
   return (
-    <View style={styles.view}>
-      <Text>This is the Settings screen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={styles.backArrow}>{"<"}</Text>
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>Announcements - View</Text>
+      </View>
+      <View style={styles.menuItem}>
+        <Text style={styles.menuItemText}>ACCOUNT CENTER</Text>
+      </View>
+      <View style={styles.menuItem}>
+        <Text style={styles.menuItemText}>LANGUAGE</Text>
+      </View>
+      <View style={styles.menuItem}>
+        <Text style={styles.menuItemText}>NOTIFICATIONS</Text>
+      </View>
+      <View style={styles.menuItem}>
+        <Text style={styles.menuItemText}>HELP</Text>
+      </View>
+      <View style={styles.menuItem}>
+        <Text style={styles.menuItemText}>ABOUT</Text>
+      </View>
+      <View style={styles.menuItem}>
+        <Text style={styles.menuItemText}>LOG OUT</Text>
+      </View>
     </View>
   )
 }
--- screens/Settings/styles.ts
+++ screens/Settings/styles.ts
@@ -1,9 +1,35 @@
 import { StyleSheet } from "react-native"
+import { black, white } from "../../assets/colors/colors"
 
 export const styles = StyleSheet.create({
-  view: {
-    alignItems: 'center',
+  backArrow: {
+    color: black,
+    fontSize: 24,
+    marginRight: 10,
+  },
+  container: {
+    backgroundColor: white,
     flex: 1,
-    justifyContent: 'center'
-  }
+  },
+  header: {
+    alignItems: "center",
+    borderBottomWidth: 2,
+    borderColor: black,
+    flexDirection: "row",
+    padding: 10,
+  },
+  headerTitle: {
+    color: black,
+    fontSize: 18,
+  },
+  menuItem: {
+    borderBottomWidth: 1,
+    borderColor: black,
+    paddingLeft: 10,
+    paddingVertical: 15,
+  },
+  menuItemText: {
+    color: black,
+    fontSize: 16,
+  },
 })
