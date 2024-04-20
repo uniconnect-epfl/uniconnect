de05077d5cb5ef96a0c0da3c9d09d2f8e328b6b6
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-20 13:36:23 +0200

    feat: implemented ExternalProfileScreen and added a more global profileSyles

--- screens/Profile/ExternalProfileScreen/ExternalProfileScreen.tsx
+++ screens/Profile/ExternalProfileScreen/ExternalProfileScreen.tsx
@@ -3,7 +3,12 @@ import { styles } from "./styles"
 import { globalStyles } from "../../../assets/global/globalStyles"
 import GeneralProfile from "../../../components/GeneralProfile/GeneralProfile"
 import ExpandableDescription from "../../../components/ExpandableDescription/ExpandableDescription"
-import { RouteProp, useRoute } from "@react-navigation/native"
+import SectionTabs from "../../../components/SectionTabs/SectionTabs"
+import { useState } from "react"
+import { profileStyles } from "../profileStyles"
+import { ProfileEvents } from "../ProfileEvents/ProfileEvents"
+import { ProfileInterests } from "../ProfileInterests/ProfileInterests"
+import { ProfileNetwork } from "../ProfileNetwork/ProfileNetwork"
 
 type Contact = {
   uid: string
@@ -19,7 +24,7 @@ type Contact = {
 const dummyProfile: Contact = {
   uid: "4",
   firstName: "Hervé",
-  lastName: "DelaMontagne",
+  lastName: "DelaStrite",
   profilePictureUrl: "",
   description: "Description, description, descrc hdsjklaf hel fhj fdj bjhd vbfdhj vbdjhl vdjlh vdhj vfdjh hvdu vdh vdfj vdfs hfdj fdj vdfjl d grei hrj heiua fjispd jis gjdrkl gjreks reksl grei gril nrsj njiption, loooooooooong descriiiiiiiiiiption, this is a veeeeeeeeeeeeeeeeeeeeeery loooooooooooooooooooooooooooooooooooooooooooong descriiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiption alala",
   interests: ["running"],
@@ -27,6 +32,7 @@ const dummyProfile: Contact = {
   location: "EPFL Ecublens"
 }
 
+/*
 type RootStackParamList = {
   ExternalProfile: {
       uid: string;
@@ -34,21 +40,20 @@ type RootStackParamList = {
 };
 
 type ExternalProfileScreenRouteProp = RouteProp<RootStackParamList, 'ExternalProfile'>;
+*/
 
+const ExternalProfileScreen = () => {
 
-const ExternalProfileScreen = (
-  //{ route } : {route: RouteProp<{ params: { uid: string } }, 'params'>}
-) => {
-
-  const { uid } = useRoute<ExternalProfileScreenRouteProp>().params
+  //const { uid } = useRoute<ExternalProfileScreenRouteProp>().params
+  const [selectedTab, setSelectedTab] = useState("Network")
   
   return (
 
     <View style={styles.container}>
-      <View style={styles.topBackground} />
-      <View style={styles.profileContainer}>
+      <View style={profileStyles.topBackground} />
+      <View style={profileStyles.profileContainer}>
 
-        <View style={styles.topProfileContainer}>
+        <View style={profileStyles.topProfileContainer}>
 
           <GeneralProfile
             name={dummyProfile.firstName}
@@ -56,32 +61,39 @@ const ExternalProfileScreen = (
             location={dummyProfile.location}
           />
           
-          <View style={styles.buttonsContainer}>
+          <View style={profileStyles.buttonsContainer}>
             <TouchableOpacity 
-              style={styles.button}
+              style={profileStyles.button}
               onPress={() => alert("To come")}>
-              <Text style={[globalStyles.boldText, styles.buttonText]}>Message</Text>
+              <Text style={[globalStyles.boldText, profileStyles.buttonText]}>Message</Text>
             </TouchableOpacity>
             <TouchableOpacity 
-              style={[styles.button, styles.invertedButtonColors]}
+              style={[profileStyles.button, styles.invertedButtonColors]}
               onPress={() => alert("To come")}>
-              <View style={styles.horizontalContainer}>
-                <Text style={[globalStyles.boldText, styles.buttonText]}>Remove</Text>
+              <View style={profileStyles.horizontalContainer}>
+                <Text style={[globalStyles.boldText, profileStyles.buttonText]}>Remove</Text>
               </View>
             </TouchableOpacity>
           </View>
 
         </View>
 
-        <ExpandableDescription
-          description={uid}
+        <ExpandableDescription description={dummyProfile.description} />
+
+        <SectionTabs 
+          tabs={["Events", "Interests", "Network"]}
+          startingTab={selectedTab}
+          onTabChange={tab => {setSelectedTab(tab)}}
         />
 
         <View style={styles.separatorLine} />
 
-        </View>
+        { selectedTab === "Events" && <ProfileEvents /> }
+        { selectedTab === "Interests" && <ProfileInterests /> }
+        { selectedTab === "Network" && <ProfileNetwork /> }
 
       </View>
+    </View>
 
   )
 }
