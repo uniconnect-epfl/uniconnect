51b72fcc04d84089aa6058e022c96b1b06c8e826
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-20 16:25:27 +0200

    test: added navigation mock in ContactList tests

--- __test__/screens/Contacts/ContactListScreen.test.tsx
+++ __test__/screens/Contacts/ContactListScreen.test.tsx
@@ -5,6 +5,17 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { black, lightGray } from '../../../assets/colors/colors'
 import { NavigationContainer } from '@react-navigation/native'
 
+const mockNavigate = jest.fn()
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual('@react-navigation/native'),
+    useNavigation: () => ({
+      navigate: mockNavigate,
+    }),
+  }
+})
+
 jest.mock('react-native-safe-area-context', () => {
   const inset = {top: 0, right: 0, bottom: 0, left: 0}
   return {
