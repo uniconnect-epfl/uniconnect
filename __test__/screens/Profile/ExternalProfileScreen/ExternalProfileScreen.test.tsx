3ca9d565af63f93a83fcaa178cfef6d8739577fd
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-20 13:40:57 +0200

    test: updated imports for ExternalProfile tests

--- __test__/screens/Profile/ExternalProfileScreen/ExternalProfileScreen.test.tsx
+++ __test__/screens/Profile/ExternalProfileScreen/ExternalProfileScreen.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react-native'
-import { ExternalProfileScreen } from '../../../../screens/Profile/ExternalProfileScreen/ExternalProfileScreen'
+import ExternalProfileScreen from '../../../../screens/Profile/ExternalProfileScreen/ExternalProfileScreen'
 
 describe('ExternalProfileScreen', () => {
   
