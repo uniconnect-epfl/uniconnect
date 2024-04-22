4021ded2de5dce08cd1298acebc9ec9c3fe26a51
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-22 23:34:47 +0200

    feat: initial layout of the qr screen

--- screens/Profile/MyQrCode/MyQrCodeScreen.tsx
+++ screens/Profile/MyQrCode/MyQrCodeScreen.tsx
@@ -1,12 +1,33 @@
-import { View } from "react-native"
+import { View, Text } from "react-native"
 import { Ionicons } from '@expo/vector-icons'
 import { black } from "../../../assets/colors/colors"
 import { styles } from "./styles"
+import { globalStyles } from "../../../assets/global/globalStyles"
+
+interface ContactData {
+  uid: string,
+  firstName: string,
+  lastName: string,
+}
+
+const dummyData : ContactData = {
+  uid: "467839564739",
+  firstName: "Gilles",
+  lastName: "Mempapeur"
+}
 
 export const MyQrCodeScreen = () => {
   return (
     <View style={styles.container}>
-        <Ionicons name='qr-code' size={200} color={black} />
+        <View style={styles.qrContainer}>
+          <View style={styles.textContainer}>
+            <Text style={globalStyles.boldText}>{dummyData.firstName}</Text>
+            <Text style={globalStyles.text}>Uniconnect contact</Text>
+          </View>
+          <View style={styles.container}>
+            <Ionicons name='qr-code' size={200} color={black} />
+          </View>
+        </View>
     </View>
   )
 }
--- screens/Profile/MyQrCode/styles.ts
+++ screens/Profile/MyQrCode/styles.ts
@@ -1,9 +1,23 @@
 import { StyleSheet } from "react-native"
+import { lightPeach } from "../../../assets/colors/colors"
 
 export const styles = StyleSheet.create({
     container: {
         alignItems: "center",
         flex: 1,
         justifyContent: "center",
+    },
+    qrContainer: {
+        alignItems: "center",
+        backgroundColor: lightPeach,
+        height: "40%",
+        justifyContent: "center",
+        width: "80%",
+    },
+    textContainer: {
+        alignItems: "center",
+        height: "30%",
+        justifyContent: "center",
     }
+
 })
