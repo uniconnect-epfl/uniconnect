5edd451752c8b1d33f19a16e3bf78da8cb196050
GustaveCharles <91184289+GustaveCharles@users.noreply.github.com>
2024-04-22 13:34:43 +0200

    feat: implement google authentication

--- __test__/screens/App.test.tsx
+++ __test__/screens/App.test.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import { render } from '@testing-library/react-native'
+import { render , waitFor} from '@testing-library/react-native'
 import App from '../../App'
+
+jest.mock('@react-native-google-signin/google-signin', () => {
+  return {
+    GoogleSignin: {
+      configure: jest.fn(),
+      signIn: jest.fn(() => Promise.resolve({
+        idToken: 'mock-id-token',
+        accessToken: 'mock-access-token',
+        user: {
+          email: 'test@example.com',
+          id: '123',
+          name: 'Test User'
+        }
+      })),
+      signOut: jest.fn(),
+    }
+  }
+})
 
 describe('App', () => {
   
-  it('renders correctly', () => {
+  it('renders correctly', async () => {
+    await waitFor(async () => {
     const component = render(<App />)
     expect(component).toBeTruthy()
+    })
   })
   
 })
