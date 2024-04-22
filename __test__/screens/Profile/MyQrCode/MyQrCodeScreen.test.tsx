a9626af8572487797f53e0a893b68c050ee8b4e3
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-22 23:35:39 +0200

    test: test for qr screen content

--- __test__/screens/Profile/MyQrCode/MyQrCodeScreen.test.tsx
+++ __test__/screens/Profile/MyQrCode/MyQrCodeScreen.test.tsx
@@ -8,5 +8,12 @@ describe('MyQrCodeScreen', () => {
     const component = render(<MyQrCodeScreen />)
     expect(component).toBeTruthy()
   })
+
+  it('renders the right information', () => {
+    const { getByText } = render(<MyQrCodeScreen />)
+
+    expect(getByText("Uniconnect contact")).toBeTruthy()
+    expect(getByText("Gilles")).toBeTruthy()
+  })
   
 })
