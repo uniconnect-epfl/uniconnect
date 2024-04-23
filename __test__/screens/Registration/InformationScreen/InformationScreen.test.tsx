409f46b3c43f002b53bdb7dfe0fc4edd126d40c2
Fyuryy <plagouveia@gmail.com>
2024-04-23 16:17:00 +0200

    fix: fixed InformationScreen tests

--- __test__/screens/Registration/InformationScreen/InformationScreen.test.tsx
+++ __test__/screens/Registration/InformationScreen/InformationScreen.test.tsx
@@ -1,42 +1,8 @@
 import React from "react"
-import { render, fireEvent, act } from "@testing-library/react-native"
+import { render, fireEvent } from "@testing-library/react-native"
 import InformationScreen from "../../../../screens/Registration/InformationScreen/InformationScreen"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { NavigationContainer } from "@react-navigation/native"
-import MyDateInputComponent from "../../../../components/DatePicker/DatePicker"
-
-describe("MyDateInputComponent", () => {
-  const mockSetDate = jest.fn()
-  const mockSetDateModal = jest.fn()
-  const testDate = new Date(2021, 9, 8) // October 8, 2021
-
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
-  it("calls setDate when the date changes", () => {
-    const { getByTestId } = render(
-      <MyDateInputComponent
-        date={testDate}
-        setDate={mockSetDate}
-        setDateModal={mockSetDateModal}
-      />
-    )
-
-    const dateTimePicker = getByTestId("dateTimePicker")
-    expect(dateTimePicker).toBeDefined()
-
-    // Simulate change event by directly invoking onChange handler
-    const selectedDate = new Date(2021, 9, 9) // New date to simulate change
-    act(() => {
-      fireEvent(dateTimePicker, "onChange", {
-        nativeEvent: { timestamp: selectedDate },
-      })
-    })
-
-    // Check if setDate is called with the updated date
-    expect(mockSetDate).toHaveBeenCalledWith(selectedDate)
-  })
-})
 
 const mockNavigate = jest.fn()
 jest.mock("@react-navigation/native", () => {
@@ -55,18 +21,6 @@ jest.mock("react-native-safe-area-context", () => {
     SafeAreaConsumer: jest.fn(({ children }) => children(inset)),
     useSafeAreaInsets: jest.fn(() => inset),
     useSafeAreaFrame: jest.fn(() => ({ x: 0, y: 0, width: 390, height: 844 })),
-  }
-})
-
-jest.mock("@react-native-community/datetimepicker", () => {
-  return {
-    __esModule: true,
-    MyDateInputComponent: jest.fn().mockImplementation(({ onChange }) => {
-      // Use a Pressable to simulate user interaction in tests
-      const handleChange = () => onChange({ type: "set" }, new Date()) // Simulate changing the date
-
-      return <div onClick={handleChange}></div>
-    }),
   }
 })
 
@@ -90,21 +44,6 @@ describe("Information Screen", () => {
     expect(getByPlaceholderText("Location"))
     expect(getByText("Use my location?"))
     expect(getByText("Add a description now"))
-  })
-
-  it.only("toggles date picker modal visibility on press", () => {
-    const { getByText, queryByText } = render(
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <InformationScreen />
-        </NavigationContainer>
-      </SafeAreaProvider>
-    )
-
-    const dateOfBirthField = getByText("Date of Birth*")
-    fireEvent.press(dateOfBirthField)
-    // Assuming MyDateInputComponent renders something identifiable
-    expect(queryByText("MyDateInputComponent")).toBeTruthy()
   })
 
   it("navigates to description up screen on footer press", () => {
