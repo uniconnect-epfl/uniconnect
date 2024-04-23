3b49258c0858c70675f229be70aaf38402131f00
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-23 10:39:14 +0200

    feat: added scan screen and link it with navigation

---
+++ screens/QrScan/QrScanScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+import { styles } from "./styles"
+
+const QrScanScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Scan Qr</Text>
+    </View>
+  )
+}
+
+export default QrScanScreen
