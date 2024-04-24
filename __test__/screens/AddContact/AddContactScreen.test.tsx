0ffe7a36e2ec5985dd833f5ed5ace5cd2c105cdf
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-25 00:21:22 +0200

    test: fixed tests

--- __test__/screens/AddContact/AddContactScreen.test.tsx
+++ __test__/screens/AddContact/AddContactScreen.test.tsx
@@ -2,6 +2,16 @@ import React from "react"
 import { render } from "@testing-library/react-native"
 import { AddContactScreen } from "../../../screens/AddContact/AddContactScreen"
 
+jest.mock("@react-navigation/native", () => {
+    const actualNav = jest.requireActual("@react-navigation/native")
+    return {
+      ...actualNav,
+      useRoute: () => ({
+        params: { uid: "123" },
+      }),
+    }
+})
+
 describe("AddContactScreen", () => {
   
   it("renders correctly", () => {
