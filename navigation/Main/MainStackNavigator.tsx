833ecc16c2e4b664961be373fa31a794b41b4024
Fyuryy <plagouveia@gmail.com>
2024-04-24 18:57:12 +0200

    fix: updated MainStackNavigator

--- navigation/Main/MainStackNavigator.tsx
+++ navigation/Main/MainStackNavigator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }from "react"
+import React, { useEffect, useState } from "react"
 import { createStackNavigator } from "@react-navigation/stack"
 import OnboardingScreen from "../../screens/Onboarding/OnboardingScreen"
 import InformationScreen from "../../screens/Registration/InformationScreen/InformationScreen"
@@ -10,90 +10,99 @@ import AuthenticationScreen from "../../screens/Registration/AuthenticationScree
 import { MyQrCodeScreen } from "../../screens/Profile/MyQrCode/MyQrCodeScreen"
 import { UpdateMyProfileScreen } from "../../screens/Profile/UpdateMyProfile/UpdateMyProfileScreen"
 import ExternalProfileScreen from "../../screens/Profile/ExternalProfileScreen/ExternalProfileScreen"
-import  DescriptionScreen  from "../../screens/Registration/DescriptionScreen/DescriptionScreen"
+import DescriptionScreen from "../../screens/Registration/DescriptionScreen/DescriptionScreen"
+import { auth } from "../../firebase/firebaseConfig"
+import { User, onAuthStateChanged } from "firebase/auth"
+import LoadingScreen from "../../screens/Loading/LoadingScreen"
 
 const Stack = createStackNavigator()
 
 // return  the StackNavigation that will be called in the app to allow the user to Login/Register
 const MainStackNavigator: React.FC = () => {
-  const [user, setUser] = useState<User|null>(null)
+  const [user, setUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(true)
-  
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if(user){
+      if (user) {
         setUser(user)
-      }
-      else{
+      } else {
         setUser(null)
       }
       setLoading(false)
     })
     return unsubscribe
-  },[])
+  }, [])
 
-  if(loading){
-    return (
-      <LoadingScreen/>
-    )
+  if (loading) {
+    return <LoadingScreen />
   }
   return (
-    <Stack.Navigator initialRouteName="Onboarding">
-      <Stack.Screen
-        name="Onboarding"
-        component={OnboardingScreen}
-        options={{ headerShown: false }} // Set options as needed, i.e hiding the header
-      />
-      <Stack.Screen
-        name="Information"
-        component={InformationScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Description"
-        component={DescriptionScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Interests"
-        component={InterestsScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Authentication"
-        component={AuthenticationScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="HomeTabs"
-        component={HomeTabNavigator}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Profile"
-        component={MyProfileScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="MyQR"
-        component={MyQrCodeScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="UpdateProfile"
-        component={UpdateMyProfileScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="ExternalProfile"
-        component={ExternalProfileScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{ headerShown: false }}
-      />
+    <Stack.Navigator>
+      {user ? (
+        <>
+          <Stack.Screen
+            name="HomeTabs"
+            component={HomeTabNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={MyProfileScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="MyQR"
+            component={MyQrCodeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="UpdateProfile"
+            component={UpdateMyProfileScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ExternalProfile"
+            component={ExternalProfileScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{ headerShown: false }}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="Onboarding"
+            component={OnboardingScreen}
+            options={{ headerShown: false }} // Set options as needed, i.e hiding the header
+          />
+          <Stack.Screen
+            name="Information"
+            component={InformationScreen}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="Description"
+            component={DescriptionScreen}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="Interests"
+            component={InterestsScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Authentication"
+            component={AuthenticationScreen}
+            options={{ headerShown: false }}
+          />
+        </>
+      )}
     </Stack.Navigator>
   )
 }
