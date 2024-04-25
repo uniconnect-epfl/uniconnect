9381ddf47b4311216b97760085ab7e9775e0e182
Wepo <alex.mourot@icloud.com>
2024-04-25 23:35:07 +0200

    feat: styling futur button

--- screens/Maps/styles.ts
+++ screens/Maps/styles.ts
@@ -1,7 +1,18 @@
 // MapStyles.ts
 import { StyleSheet } from 'react-native'
+import { white } from '../../assets/colors/colors'
 
 export default StyleSheet.create({
+	backButton: {
+		
+		backgroundColor: white, // Change as needed
+		borderRadius: 5,
+		left: 10,
+		padding: 10,
+		position: 'absolute',
+		top: 10,
+		zIndex: 10, // Make sure the button is above the map
+	},
 	calloutTextLocation: {
         fontSize: 14,
     },
