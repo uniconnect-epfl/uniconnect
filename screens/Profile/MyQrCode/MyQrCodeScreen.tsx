3cf38e3d8372ef3cbc224072c477b5b2f6d8599e
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-25 17:18:06 +0200

    feat: qr code generation and display (#118)
    * feat: initial layout of the qr screen
    
    * test: test for qr screen content
    
    * feat: added qr code lib dependency
    
    * feat: dummy qr code implementation
    
    * fix: added updated packate-lock.json
    
    * feat: improved styles
    
    * feat: the qr codes are now deep links into the app
    
    * feat: added AddContactScreen for after scanning a qr code
    
    * test: added test file for AddContactScreen
    
    * test: fixed tests
    
    * feat: added deep linking into the app
    
    * test: added linking mock in app test

--- screens/Profile/MyQrCode/MyQrCodeScreen.tsx
+++ screens/Profile/MyQrCode/MyQrCodeScreen.tsx
@@ -1,12 +1,48 @@
-import { View } from "react-native"
-import { Ionicons } from '@expo/vector-icons'
-import { black } from "../../../assets/colors/colors"
+import React from "react"
+import { View, Text } from "react-native"
 import { styles } from "./styles"
+import { globalStyles } from "../../../assets/global/globalStyles"
+import QRCode from "react-native-qrcode-svg"
+import { black, lightPeach, peach } from "../../../assets/colors/colors"
+import * as Linking from 'expo-linking'
+
+interface ContactData {
+  uid: string,
+  firstName: string,
+  lastName: string,
+}
+
+const dummyData : ContactData = {
+  uid: "123",
+  firstName: "Gilles",
+  lastName: "Mempapeur"
+}
+
+const generateLink = (contactData : ContactData) => {
+  return Linking.createURL("uniconnect/contact/" + contactData.uid)
+}
 
 export const MyQrCodeScreen = () => {
   return (
     <View style={styles.container}>
-        <Ionicons name='qr-code' size={200} color={black} />
+        <View style={styles.contactContainer}>
+          <View style={styles.textContainer}>
+            <Text style={globalStyles.boldText}>{dummyData.firstName}</Text>
+            <Text style={globalStyles.text}>Uniconnect contact</Text>
+          </View>
+          <View style={styles.qrContainer}>
+            <QRCode
+              size={200}
+              color={black}
+              backgroundColor={lightPeach}
+              logo={require("../../../assets/icon.png")}
+              logoBackgroundColor={peach}
+              logoBorderRadius={10}
+              logoSize={30}
+              value={generateLink(dummyData)}
+            />
+          </View>
+        </View>
     </View>
   )
 }
