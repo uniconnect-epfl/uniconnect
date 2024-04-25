681fae412fde6e4c8dd1208bdaac53d75e581caf
Fyuryy <91114548+Fyuryy@users.noreply.github.com>
2024-04-25 20:01:39 +0200

    feat: adding labels to interest screen (#122)
    * feat: added Label component
    
    * feat: implemented labels in Interest Screen
    
    The labels are not yet fully functional, but we can have an idea of how they'll look like
    
    * feat: finished labels
    
    * test: tested Label component
    
    * test: added tests in InterestScreen
    
    Tested label interactions in InterestScreen
    
    * fix: changed style files

---
+++ components/Label/Label.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { View, Text, TouchableOpacity } from "react-native"
+import { Entypo } from "@expo/vector-icons"
+import { black } from "../../assets/colors/colors"
+import styles from "./styles"
+import { globalStyles } from "../../assets/global/globalStyles"
+
+interface LabelProps {
+  text: string
+  onClick: () => void
+}
+
+const Label: React.FC<LabelProps> = ({ text, onClick }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={onClick}
+        style={styles.buttonView}
+        testID={`label-${text}`}
+      >
+        <Entypo name="cross" color={black} size={16} testID="icon-cross" />
+        <Text style={styles.text}>
+          <Text style={[globalStyles.text, styles.text]}>{text}</Text>
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default Label
--- screens/Registration/InterestsScreen/InterestsScreen.tsx
+++ screens/Registration/InterestsScreen/InterestsScreen.tsx
@@ -9,12 +9,14 @@ import {
   ListRenderItemInfo,
   Keyboard,
   TouchableWithoutFeedback,
+  ScrollView,
 } from "react-native"
 import styles from "./styles"
 import "../../../assets/global/globalStyles"
 import { globalStyles } from "../../../assets/global/globalStyles"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import LowBar from "../../../components/LowBar/LowBar"
+import Label from "../../../components/Label/Label"
 
 interface InterestButtonProps {
   title: string
@@ -30,6 +32,7 @@ const InterestButton: React.FC<InterestButtonProps> = ({
   <TouchableOpacity
     style={[styles.interestButton, selected && styles.selectedInterestButton]}
     onPress={onSelect}
+    testID={`interestButton-${title}`}
   >
     <Text
       style={[
@@ -67,6 +70,8 @@ const InterestsScreen = () => {
   const [searchTerm, setSearchTerm] = useState("")
   const [filterdedInterests, setFilteredInterests] = useState(interests)
   const [selectedInterests, setSelectedInterests] = useState(new Set())
+  //array of selected interests
+  const [labelArray, setLabelArray] = useState([] as string[])
 
   const renderItem = ({ item }: ListRenderItemInfo<string>) => (
     <InterestButton
@@ -76,13 +81,25 @@ const InterestsScreen = () => {
     />
   )
 
+  const handleRemoveInterest = (interest: string) => {
+    setSelectedInterests((prev) => {
+      const newSelectedInterests = new Set(prev)
+      newSelectedInterests.delete(interest)
+      return newSelectedInterests
+    })
+    setLabelArray((prev) => prev.filter((label) => label !== interest))
+  }
+
   const toggleInterest = (interest: string) => {
     setSelectedInterests((prev) => {
       const newSelectedInterests = new Set(prev)
       if (newSelectedInterests.has(interest)) {
         newSelectedInterests.delete(interest)
+        // Build a new array without the interest
+        setLabelArray((prev) => prev.filter((label) => label !== interest))
       } else {
         newSelectedInterests.add(interest)
+        setLabelArray([...labelArray, interest])
       }
       return newSelectedInterests
     })
@@ -116,13 +133,28 @@ const InterestsScreen = () => {
           Select your interests
         </Text>
 
-        <View>
-          <TextInput
-            placeholder="Search"
-            style={[styles.input, globalStyles.text]}
-            onChangeText={handleSearch}
-          />
-        </View>
+        <TextInput
+          placeholder="Search"
+          style={[styles.input, globalStyles.text]}
+          onChangeText={handleSearch}
+        />
+
+        {selectedInterests.size !== 0 && (
+          <ScrollView
+            horizontal={false}
+            style={styles.labelView}
+            showsHorizontalScrollIndicator={true}
+            contentContainerStyle={styles.labelContainer}
+          >
+            {labelArray.map((label) => (
+              <Label
+                key={label}
+                text={label}
+                onClick={() => handleRemoveInterest(label)}
+              />
+            ))}
+          </ScrollView>
+        )}
 
         <FlatList
           data={filterdedInterests}
