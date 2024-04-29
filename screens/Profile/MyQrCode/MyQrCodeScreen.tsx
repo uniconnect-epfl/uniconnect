a96b8961339d05554e517ab8c8dd810b679c0de1
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-29 16:14:49 +0200

    feat: put real user data in the personnal qr code

--- screens/Profile/MyQrCode/MyQrCodeScreen.tsx
+++ screens/Profile/MyQrCode/MyQrCodeScreen.tsx
@@ -1,33 +1,44 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { View, Text } from "react-native"
 import { styles } from "./styles"
 import { globalStyles } from "../../../assets/global/globalStyles"
 import QRCode from "react-native-qrcode-svg"
 import { black, lightPeach, peach } from "../../../assets/colors/colors"
 import * as Linking from 'expo-linking'
+import { getAuth } from "firebase/auth"
+import { getUserData } from "../../../firebase/User"
+import LoadingScreen from "../../Loading/LoadingScreen"
+import { User } from "../../../types/User"
 
-interface ContactData {
-  uid: string,
-  firstName: string,
-  lastName: string,
-}
-
-const dummyData : ContactData = {
-  uid: "123",
-  firstName: "Gilles",
-  lastName: "Mempapeur"
-}
-
-const generateLink = (contactData : ContactData) => {
-  return Linking.createURL("uniconnect/contact/" + contactData.uid)
+const generateLink = (uid: string) => {
+  return Linking.createURL("uniconnect/contact/" + uid)
 }
 
 export const MyQrCodeScreen = () => {
+  const userId = getAuth().currentUser?.uid
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      if(userId){
+        setUser(await getUserData(userId))
+      }
+      setLoading(false)
+    }
+    fetchData()
+  }, [userId])
+
+  if(loading || !user){
+    return <LoadingScreen/>
+  }
+  
   return (
     <View style={styles.container}>
         <View style={styles.contactContainer}>
           <View style={styles.textContainer}>
-            <Text style={globalStyles.boldText}>{dummyData.firstName}</Text>
+            <Text style={globalStyles.boldText}>{user.firstName}</Text>
             <Text style={globalStyles.text}>Uniconnect contact</Text>
           </View>
           <View style={styles.qrContainer}>
@@ -39,7 +50,7 @@ export const MyQrCodeScreen = () => {
               logoBackgroundColor={peach}
               logoBorderRadius={10}
               logoSize={30}
-              value={generateLink(dummyData)}
+              value={generateLink(user.uid)}
             />
           </View>
         </View>
