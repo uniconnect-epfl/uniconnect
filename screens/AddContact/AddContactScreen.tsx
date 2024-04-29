2acb256d4b8fb14409d96e1efc481768d5581609
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-04-29 21:05:05 +0200

    feat: made very basic add contact screen

--- screens/AddContact/AddContactScreen.tsx
+++ screens/AddContact/AddContactScreen.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react"
-import { View, Text } from "react-native"
+import { View, Text, Image, TouchableOpacity, Alert } from "react-native"
 import { styles } from "./styles"
 import { RouteProp, useRoute } from "@react-navigation/native"
 import { User } from "../../types/User"
 import LoadingScreen from "../Loading/LoadingScreen"
 import { getUserData } from "../../firebase/User"
+import { Ionicons } from "@expo/vector-icons"
+import { black } from "../../assets/colors/colors"
+import { globalStyles } from "../../assets/global/globalStyles"
+
+const userProfilePictureUrl = ""
 
 type RootStackParamList = {
   AddContactScreen: {
@@ -36,7 +41,39 @@ export const AddContactScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text>{user.firstName} {user.lastName}</Text>
+
+      <View style={styles.profilePictureContainer}>
+        {userProfilePictureUrl ? (
+          <Image
+            style={styles.profilePicture}
+            source={{ uri: userProfilePictureUrl }}
+          />
+        ) : (
+          <View style={styles.profilePicture}>
+            <Ionicons name="person" size={130} color={black} />
+          </View>
+        )}
+      </View>
+
+      <View style={styles.infosContainer}>
+        <Text style={globalStyles.boldText}>{user.firstName} {user.lastName}</Text>
+        <View style={styles.horizontalContainer}>
+            <Ionicons name="location-outline" size={13} color={black} />
+            <Text style={globalStyles.smallText}>{user.location}</Text>
+        </View>
+        <Text style={[globalStyles.smallText, styles.description]}>{user.description}</Text>
+      </View>
+
+      <View style={styles.addButtonContainer}>
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => {
+            Alert.alert("Not implemented yet")
+          }}>
+            <Text style={globalStyles.boldText}>Add to contacts</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
