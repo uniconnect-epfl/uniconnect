15b51262a21b580b269fdfc7006920e5cb553952
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-05-01 23:26:50 +0200

    feat: add contact by qr (#133)
    * feat: put real user data in the personnal qr code
    
    * test: updated tests to reflect changes
    
    * fix: fixed qr code generation path for contacts
    
    * feat: linked scanning qr code to the adding contact screen
    
    * feat: made very basic add contact screen
    
    * docs: added comments
    
    * test: fixed some of the tests to account for changes
    
    * test: removed one problematic test
    
    * test: added coverage
    
    * test: tried to add coverage again
    
    * test: completed add contact screen tests
    
    * refactor: small style fixes
    
    * feat: added dependency use-debounce
    
    * feat: added debouncer when scanning qr code
    
    * test: added test for camera debouncer
    
    * test: improved tests for camera debouncer

--- screens/AddContact/styles.ts
+++ screens/AddContact/styles.ts
@@ -1,9 +1,52 @@
 import { StyleSheet } from "react-native"
+import { lightPeach, peach } from "../../assets/colors/colors"
 
 export const styles = StyleSheet.create({
+    addButton: {
+        backgroundColor: lightPeach,
+        borderColor: peach,
+        borderRadius: 20,
+        borderWidth: 2,
+        elevation: 6,
+        paddingHorizontal: 30,
+        paddingVertical: 20,
+    },
+    addButtonContainer: {
+        alignItems: "center",
+        flex: 0.3,
+        justifyContent: "center",
+    },
     container: {
         alignItems: "center",
         flex: 1,
         justifyContent: "center",
-    }
+    },
+    description: {
+        margin: 20,
+    },
+    horizontalContainer: {
+        flexDirection: "row",
+        width: "100%",
+    },
+    infosContainer: {
+        alignItems: "center",
+        flex: 0.4,
+        justifyContent: "flex-start",
+    },
+    profilePicture: {
+        alignItems: "center",
+        backgroundColor: peach,
+        borderRadius: 100,
+        height: 200,
+        justifyContent: "center",
+        margin: 20,
+        width: 200,
+    },
+    profilePictureContainer: {
+        alignItems: "center",
+        flex: 0.3,
+        justifyContent: "center",
+        paddingBottom: "10%",
+        paddingTop: "30%",
+    },
 })
