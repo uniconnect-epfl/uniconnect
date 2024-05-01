465ef608c7af819160a394b704ea577479803d88
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-05-01 18:04:25 +0200

    refactor: added space for conflicts ?

--- navigation/Main/MainStackNavigator.tsx
+++ navigation/Main/MainStackNavigator.tsx
@@ -146,6 +146,7 @@ const MainStackNavigator: React.FC = () => {
               component={EventQrCodeScreen}
               options={{ headerShown: false }}
             />
+            
           </>
         ) : (
           <>
