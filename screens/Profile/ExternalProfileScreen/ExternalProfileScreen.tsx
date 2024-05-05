33a1a64f131c19471934faa773ffb5ddb136f999
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-05-05 15:59:27 +0200

    feat: cleaned setIsFriend

--- screens/Profile/ExternalProfileScreen/ExternalProfileScreen.tsx
+++ screens/Profile/ExternalProfileScreen/ExternalProfileScreen.tsx
@@ -62,7 +62,7 @@ const ExternalProfileScreen = () => {
   useEffect(() => {
     if(user && externalUser){
       // this will just check if the two are friends when we'll have implemented friends
-      setIsFriend(!(externalUser.uid === "27tGkyeWaKYE46AyR8UMKwxLspH3"))
+      setIsFriend(true)
     } else {
       setIsFriend(false)
     }
