fc0f6026c905be2275ee8a5732ffce1ef25327e6
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-05-05 11:42:33 +0200

    test: added navigation mock in tests

--- __test__/screens/ViewDetails/ViewAnnouncementScreen/ViewAnnouncementScreen.test.tsx
+++ __test__/screens/ViewDetails/ViewAnnouncementScreen/ViewAnnouncementScreen.test.tsx
@@ -2,6 +2,16 @@ import React from "react"
 import { render } from "@testing-library/react-native"
 import ViewAnnoucementScreen from "../../../../screens/ViewDetails/ViewAnnouncementScreen/ViewAnnouncementScreen"
 
+jest.mock("@react-navigation/native", () => {
+    const actualNav = jest.requireActual("@react-navigation/native")
+    return {
+      ...actualNav,
+      useRoute: () => ({
+        params: { uid: "123" },
+      }),
+    }
+})
+
 describe("ViewAnnouncementScreen", () => {
   
   it("renders correctly", () => {
--- __test__/screens/ViewDetails/ViewEventScreen/ViewEventScreen.test.tsx
+++ __test__/screens/ViewDetails/ViewEventScreen/ViewEventScreen.test.tsx
@@ -2,6 +2,16 @@ import React from "react"
 import { render } from "@testing-library/react-native"
 import ViewEventScreen from "../../../../screens/ViewDetails/ViewEventScreen/ViewEventScreen"
 
+jest.mock("@react-navigation/native", () => {
+    const actualNav = jest.requireActual("@react-navigation/native")
+    return {
+      ...actualNav,
+      useRoute: () => ({
+        params: { uid: "123" },
+      }),
+    }
+})
+
 describe("ViewEventScreen", () => {
   
   it("renders correctly", () => {
