35c5a974c19863a7811c0aecaee413e56d895ed6
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-05-05 12:10:32 +0200

    feat: view annoucement screen implementation

--- screens/ViewDetails/ViewAnnouncementScreen/ViewAnnouncementScreen.tsx
+++ screens/ViewDetails/ViewAnnouncementScreen/ViewAnnouncementScreen.tsx
@@ -1,20 +1,56 @@
-import React, { View, Text } from 'react-native'
+import React, { View, Text, Alert, TouchableOpacity } from 'react-native'
 import { RouteProp, useRoute } from '@react-navigation/native'
+import { Announcement } from '../../../types/Annoucements'
+import { styles } from './styles'
+import { viewDetailsStyles } from '../ViewDetailsStyles'
+import { globalStyles } from '../../../assets/global/globalStyles'
+import ProfilePicture from '../../../components/ProfilePicture/ProfilePicture'
 
 type RootStackParamList = {
   ViewAnnouncementScreen: {
-      uid: string;
+      announcement: Announcement;
   }
 }
 
 type ViewAnnoucementScreenRouteProps = RouteProp<RootStackParamList, "ViewAnnouncementScreen">
 
 const ViewAnnoucementScreen = () => {
-  const { uid } = useRoute<ViewAnnoucementScreenRouteProps>().params
+  const { announcement } = useRoute<ViewAnnoucementScreenRouteProps>().params
 
   return (
-    <View>
-      <Text>This is the view annoucment screen, uid: {uid}</Text>
+    <View style={styles.container}>
+        <View style={viewDetailsStyles.topBackground} />
+        <View style={viewDetailsStyles.detailsContainer}>
+
+            <Text style={[
+                    globalStyles.boldText, 
+                    viewDetailsStyles.title,
+                    viewDetailsStyles.detailsText
+                ]}>
+                {announcement.title}
+            </Text>
+            <Text style={[globalStyles.text, viewDetailsStyles.detailsText]}>
+                Travel - Holidays - Work
+            </Text>
+
+            <TouchableOpacity 
+                style={viewDetailsStyles.profileContainer}
+                onPress={() => {Alert.alert("Coming soon")}}>
+                    <Text style={globalStyles.smallText}>By </Text>
+                    <ProfilePicture
+                        size={25}
+                        pictureUrl=""
+                    />
+                    <Text style={globalStyles.smallText}> name surname</Text>
+            </TouchableOpacity>
+
+            <View style={viewDetailsStyles.separationBar}/>
+
+            <Text style={[globalStyles.smallText, viewDetailsStyles.descriptionContainer]}>
+                {announcement.description}
+            </Text>
+            
+        </View>
     </View>
   )
 }
--- screens/ViewDetails/ViewDetailsStyles.ts
+++ screens/ViewDetails/ViewDetailsStyles.ts
@@ -1,14 +1,37 @@
 import { StyleSheet } from "react-native" 
-import { lightPeach } from "../../assets/colors/colors"
+import { black, lightPeach } from "../../assets/colors/colors"
 
-export const styles = StyleSheet.create({
-    profileContainer: {
+export const viewDetailsStyles = StyleSheet.create({
+    descriptionContainer: {
+        margin: "5%",
+    },
+    detailsContainer: {
         flex: 1,
+        margin: "5%",
+        width: "90%",
+    },
+    detailsText: {
+        marginVertical: 3,
+        textAlign: "left",
+    },
+    profileContainer: {
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        marginVertical: 3,
+    },
+    separationBar: {
+        backgroundColor: black,
+        borderRadius: 1,
+        height: 1,
+        marginTop: 8,
+    },
+    title: {
+        fontSize: 30,
     },
     topBackground: {
         backgroundColor: lightPeach,
         height: "20%",
-        position: "absolute",
         top: 0,
         width: "100%"
     },
