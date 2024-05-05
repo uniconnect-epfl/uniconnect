e1e1b2d003f9b79b96c883486b32b39a9e196e85
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-05-05 11:22:30 +0200

    feat: added profilePicture component

---
+++ components/ProfilePicture/ProfilePicture.tsx
@@ -0,0 +1,28 @@
+import { Ionicons } from "@expo/vector-icons"
+import React, {View, Image} from "react-native"
+import { black } from "../../assets/colors/colors"
+import { styles } from "./styles"
+
+interface ProfilePictureProps {
+  size: number
+  pictureUrl: string
+}
+
+const ProfilePicture = ({ size, pictureUrl }: ProfilePictureProps) => {
+  return (
+    <View>
+        {pictureUrl ? (
+            <Image
+              style={[styles.profilePicture, {width: size, height: size}]}
+              source={{ uri: pictureUrl }}
+            />
+        ) : (
+            <View style={[styles.profilePicture, {width: size, height: size}]}>
+              <Ionicons name="person" size={size} color={black} />
+            </View>
+        )}
+    </View>
+  )
+}
+
+export default ProfilePicture
