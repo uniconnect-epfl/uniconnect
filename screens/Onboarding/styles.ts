2bee3e67a979bfc93223dad71b38a94e7f680e0e
Fyuryy <plagouveia@gmail.com>
2024-05-07 11:30:29 +0200

    fix: fixed keyboard display
    Now the keyboard hides the components underneath, instead of moving them

--- screens/Onboarding/styles.ts
+++ screens/Onboarding/styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native' 
+import { StyleSheet } from "react-native"
 import {
   white,
   black,
@@ -6,58 +6,58 @@ import {
   lightPeach,
   peach,
   shadowColor,
-} from '../../assets/colors/colors' 
+} from "../../assets/colors/colors"
 
 import {
   BUTTON_HEIGHT,
   BUTTON_RADIUS,
   BUTTON_TEXT_FONT_SIZE,
-} from '../../assets/global/constants' 
+} from "../../assets/global/constants"
 
 const styles = StyleSheet.create({
   border: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
+    flexDirection: "column",
+    justifyContent: "space-between",
   },
   button: {
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: lightPeach,
     borderColor: peach,
     borderRadius: BUTTON_RADIUS,
     borderWidth: 2,
     elevation: 8,
     height: BUTTON_HEIGHT,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginBottom: 10,
     shadowColor,
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 5,
-    width: '100%',
+    width: "100%",
   },
   buttonGoogle: {
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: white,
     borderRadius: BUTTON_RADIUS,
     borderWidth: 1,
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     height: BUTTON_HEIGHT,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
     paddingLeft: 15,
     shadowColor,
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 5,
-    width: '100%',
+    width: "100%",
   },
 
   buttonPlaceholder: {
-    alignItems: 'center',
+    alignItems: "center",
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
+    flexDirection: "row",
+    justifyContent: "flex-start",
   },
   buttonText: {
     color: black,
@@ -66,19 +66,19 @@ const styles = StyleSheet.create({
   buttonTextGoogle: {
     color: black,
     fontSize: BUTTON_TEXT_FONT_SIZE,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   container: {
-    alignItems: 'center',
+    alignItems: "center",
     flex: 1,
     gap: 1,
     padding: 20,
   },
   footer: {
-    display: 'flex',
-    flexDirection: 'row',
-    position: 'absolute',
-
+    display: "flex",
+    flexDirection: "row",
+    position: "absolute",
+    zIndex: -1,
   },
   icon: {
     paddingRight: 25,
@@ -97,18 +97,18 @@ const styles = StyleSheet.create({
     height: BUTTON_HEIGHT,
     marginBottom: 20,
     padding: 10,
-    width: '100%',
+    width: "100%",
   },
   smallText: {
     color: black,
     fontSize: 10,
     marginBottom: 20,
-    textDecorationLine: 'underline',
+    textDecorationLine: "underline",
   },
   specialText: {
     color: peach,
     fontSize: 10,
   },
-}) 
+})
 
-export default styles 
+export default styles
