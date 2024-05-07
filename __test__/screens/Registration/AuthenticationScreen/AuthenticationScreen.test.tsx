87e0e3b40a8784f6386f1b62151a47f88c918144
Fyuryy <plagouveia@gmail.com>
2024-05-07 20:11:42 +0200

    fix: fixed minor error on submitForm

--- __test__/screens/Registration/AuthenticationScreen/AuthenticationScreen.test.tsx
+++ __test__/screens/Registration/AuthenticationScreen/AuthenticationScreen.test.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import { render, fireEvent, act, waitFor } from '@testing-library/react-native'
-import AuthenticationScreen from '../../../../screens/Registration/AuthenticationScreen/AuthenticationScreen'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { NavigationContainer } from '@react-navigation/native'
-import * as registration from '../../../../firebase/Registration'
-import { showErrorToast } from '../../../../components/ToastMessage/toast'
+import React from "react"
+import { render, fireEvent, act, waitFor } from "@testing-library/react-native"
+import AuthenticationScreen from "../../../../screens/Registration/AuthenticationScreen/AuthenticationScreen"
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import { NavigationContainer } from "@react-navigation/native"
+import * as registration from "../../../../firebase/Registration"
+import { showErrorToast } from "../../../../components/ToastMessage/toast"
 
-
-jest.mock('react-native-safe-area-context', () => {
+jest.mock("react-native-safe-area-context", () => {
   const inset = { top: 0, right: 0, bottom: 0, left: 0 }
   return {
     SafeAreaProvider: jest.fn(({ children }) => children),
@@ -18,28 +17,24 @@ jest.mock('react-native-safe-area-context', () => {
 })
 
 // Mocking modules
-jest.mock('../../../../firebase/Registration', () => ({
+jest.mock("../../../../firebase/Registration", () => ({
   createAccount: jest.fn(),
-  storeEmail: jest.fn()
+  storeEmail: jest.fn(),
 }))
-
 
 jest.mock("../../../../components/ToastMessage/toast", () => ({
   showErrorToast: jest.fn(),
-  showSuccessToast: jest.fn()
+  showSuccessToast: jest.fn(),
 }))
 
-
-
-describe('AuthenticationScreen', () => {
+describe("AuthenticationScreen", () => {
   beforeEach(() => {
     // Clear all mocks before each test
-    jest.clearAllMocks();
-    (registration.createAccount as jest.Mock).mockResolvedValue(void 0)
-
+    jest.clearAllMocks()
+    ;(registration.createAccount as jest.Mock).mockResolvedValue(void 0)
   })
 
-  it('renders the screen', () => {
+  it("renders the screen", () => {
     const { getByText } = render(
       <SafeAreaProvider>
         <NavigationContainer>
@@ -47,12 +42,12 @@ describe('AuthenticationScreen', () => {
         </NavigationContainer>
       </SafeAreaProvider>
     )
-    expect(getByText('Last Step')).toBeTruthy()
+    expect(getByText("Last Step")).toBeTruthy()
     expect(getByText("E-mail*")).toBeTruthy()
     expect(getByText("Confirm e-mail*")).toBeTruthy()
   })
 
-  it('allows entering passwords and emails', () => {
+  it("allows entering passwords and emails", () => {
     const { getByPlaceholderText, getAllByPlaceholderText } = render(
       <SafeAreaProvider>
         <NavigationContainer>
@@ -61,15 +56,15 @@ describe('AuthenticationScreen', () => {
       </SafeAreaProvider>
     )
     const passwordInput = getAllByPlaceholderText("****************")[0]
-    fireEvent.changeText(passwordInput, 'password123')
-    expect(passwordInput.props.value).toBe('password123')
+    fireEvent.changeText(passwordInput, "password123")
+    expect(passwordInput.props.value).toBe("password123")
 
-    const emailInput = getByPlaceholderText('E-mail')
-    fireEvent.changeText(emailInput, 'test@example.com')
-    expect(emailInput.props.value).toBe('test@example.com')
+    const emailInput = getByPlaceholderText("E-mail")
+    fireEvent.changeText(emailInput, "test@example.com")
+    expect(emailInput.props.value).toBe("test@example.com")
   })
 
-  it('validates password correctness', () => {
+  it("validates password correctness", () => {
     const { getAllByPlaceholderText, getByPlaceholderText, getByText } = render(
       <SafeAreaProvider>
         <NavigationContainer>
@@ -78,15 +73,24 @@ describe('AuthenticationScreen', () => {
       </SafeAreaProvider>
     )
 
-    fireEvent.changeText(getAllByPlaceholderText("****************")[0], 'password123')
-    fireEvent.changeText(getAllByPlaceholderText("****************")[1], 'password123')
-    fireEvent.changeText(getByPlaceholderText("E-mail"), 'test@example.com')
-    fireEvent.changeText(getByPlaceholderText("Confirm your e-mail"), 'test@example.com')
+    fireEvent.changeText(
+      getAllByPlaceholderText("****************")[0],
+      "password123"
+    )
+    fireEvent.changeText(
+      getAllByPlaceholderText("****************")[1],
+      "password123"
+    )
+    fireEvent.changeText(getByPlaceholderText("E-mail"), "test@example.com")
+    fireEvent.changeText(
+      getByPlaceholderText("Confirm your e-mail"),
+      "test@example.com"
+    )
 
-    expect(getByText('Passwords matching')).toBeTruthy()
+    expect(getByText("Passwords matching")).toBeTruthy()
   })
 
-  it('submits the form when data is correct', async () => {
+  it("submits the form when data is correct", async () => {
     const { getByText, getByPlaceholderText, getAllByPlaceholderText } = render(
       <SafeAreaProvider>
         <NavigationContainer>
@@ -95,23 +99,31 @@ describe('AuthenticationScreen', () => {
       </SafeAreaProvider>
     )
 
-    fireEvent.changeText(getAllByPlaceholderText("****************")[0], 'password123')
-    fireEvent.changeText(getAllByPlaceholderText("****************")[1], 'password123')
-    fireEvent.changeText(getByPlaceholderText("E-mail"), 'test@example.com')
-    fireEvent.changeText(getByPlaceholderText("Confirm your e-mail"), 'test@example.com')
+    fireEvent.changeText(
+      getAllByPlaceholderText("****************")[0],
+      "password123*"
+    )
+    fireEvent.changeText(
+      getAllByPlaceholderText("****************")[1],
+      "password123*"
+    )
+    fireEvent.changeText(getByPlaceholderText("E-mail"), "test@example.com")
+    fireEvent.changeText(
+      getByPlaceholderText("Confirm your e-mail"),
+      "test@example.com"
+    )
 
-    const submitButton = getByText('Confirm')
-    await  act(async () => {  
-    fireEvent.press(submitButton)
-    }) 
+    const submitButton = getByText("Confirm")
+    await act(async () => {
+      fireEvent.press(submitButton)
+    })
 
     await waitFor(() => {
       expect(registration.createAccount).toHaveBeenCalled()
     })
-
   })
 
-  it('shows alert when data is incorrect', async () => {
+  it("shows alert when data is incorrect", async () => {
     // Set up component with invalid data conditions
     const { getByText, getByPlaceholderText, getAllByPlaceholderText } = render(
       <SafeAreaProvider>
@@ -121,22 +133,32 @@ describe('AuthenticationScreen', () => {
       </SafeAreaProvider>
     )
 
-    fireEvent.changeText(getAllByPlaceholderText("****************")[0], 'password123')
-    fireEvent.changeText(getAllByPlaceholderText("****************")[1], 'password124')
-    fireEvent.changeText(getByPlaceholderText("E-mail"), 'test@example.com')
-    fireEvent.changeText(getByPlaceholderText("Confirm your e-mail"), 'test@example.com')
+    fireEvent.changeText(
+      getAllByPlaceholderText("****************")[0],
+      "password123"
+    )
+    fireEvent.changeText(
+      getAllByPlaceholderText("****************")[1],
+      "password124"
+    )
+    fireEvent.changeText(getByPlaceholderText("E-mail"), "test@example.com")
+    fireEvent.changeText(
+      getByPlaceholderText("Confirm your e-mail"),
+      "test@example.com"
+    )
 
-    const submitButton = getByText('Confirm')
+    const submitButton = getByText("Confirm")
 
     // Assuming the function to check data validity returns false
     // You might need to mock these if they are not part of the component's direct logic
 
-    await  act(async () => {  
+    await act(async () => {
       fireEvent.press(submitButton)
-      }) 
+    })
     await waitFor(() => {
-      expect(showErrorToast).toHaveBeenCalledWith("Please fill in the form correctly and try again")
+      expect(showErrorToast).toHaveBeenCalledWith(
+        "Please fill in the form correctly and try again"
+      )
     })
   })
-
 })
