dd62042ce5e90e7885248820671b80a0c4bee7d8
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-05-13 13:43:58 +0200

    feat: cleaned back arrow component

--- components/BackArrow/BackArrow.tsx
+++ components/BackArrow/BackArrow.tsx
@@ -1,18 +1,19 @@
 import React from "react"
-import { View, TouchableOpacity } from "react-native"
+import { TouchableOpacity } from "react-native"
 import { styles } from "./styles"
 import { useNavigation } from "@react-navigation/native"
 import { Ionicons } from "@expo/vector-icons"
 import { peach } from "../../assets/colors/colors"
 
-export const SettingsScreen = () => {
+export const BackArrow = () => {
   const navigation = useNavigation()
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.goBack()} testID="back-arrow">
-          <Ionicons name="arrow-back-outline" size={24} color={peach} />
-        </TouchableOpacity>
-    </View>
+    <TouchableOpacity 
+      style={styles.arrow}
+      onPress={() => navigation.goBack()} 
+      testID="back-arrow">
+        <Ionicons name="arrow-back-outline" size={24} color={peach} />
+    </TouchableOpacity>
   )
 }
--- components/BackArrow/styles.ts
+++ components/BackArrow/styles.ts
@@ -1,8 +1,12 @@
 import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center"
+  arrow: {
+    left: 0,
+    marginHorizontal: 10,
+    marginVertical: 20,
+    padding: 15,
+    position: "absolute",
+    top: 0,
   },
 })
