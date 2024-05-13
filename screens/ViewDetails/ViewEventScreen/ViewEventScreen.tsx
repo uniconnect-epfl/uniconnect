14945ca679870323e02f491e09bb4e01bcd141dc
GaelCondeLosada <93329823+GaelCondeLosada@users.noreply.github.com>
2024-05-13 22:18:20 +0200

    feat: add a button for a user to be able to register to an event (#183)
    * fix: fixed issues with clicking when scrolling
    
    * feat: modified view event layout a bit
    
    * test: added test for participate button
    
    * feat: cleared layout for participate button
    
    * test: added alert mock
    
    * feat: cleaned back arrow component
    
    * feat: added back arrow on view anouncement and event screen
    
    * test: added test for back arrow componentW
    
    * test: adapted tests of view event and annoucement

--- screens/ViewDetails/ViewEventScreen/ViewEventScreen.tsx
+++ screens/ViewDetails/ViewEventScreen/ViewEventScreen.tsx
@@ -9,6 +9,7 @@ import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import { globalStyles } from '../../../assets/global/globalStyles'
 import ProfilePicture from '../../../components/ProfilePicture/ProfilePicture'
 import { getEventData } from '../../../firebase/ManageEvents'
+import { BackArrow } from '../../../components/BackArrow/BackArrow'
 
 type RootStackParamList = {
     ViewEvent: {
@@ -42,7 +43,10 @@ const ViewEventScreen = () => {
 
   return (
     <View style={styles.container}>
+        
         <View style={viewDetailsStyles.topBackground} />
+        <BackArrow/>
+
         <View style={viewDetailsStyles.detailsContainer}>
 
             <Text style={[
@@ -98,6 +102,14 @@ const ViewEventScreen = () => {
             <Text style={[globalStyles.smallText, viewDetailsStyles.descriptionContainer]}>
                 {event.description}
             </Text>
+
+            <TouchableOpacity
+                style={styles.participateButton}
+                onPress={() => {alert("Not implemented yet")}}>
+                <Text style={globalStyles.boldText}>
+                    Participate
+                </Text>
+            </TouchableOpacity>
             
         </View>
     </View>
