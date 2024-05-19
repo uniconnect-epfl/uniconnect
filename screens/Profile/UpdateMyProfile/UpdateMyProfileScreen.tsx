684835047e07eb71e317621af33df4cc9cf464d5
Aidasvenc <79483254+Aidasvenc@users.noreply.github.com>
2024-05-19 19:20:21 +0200

    feat: update my profile screen

--- screens/Profile/UpdateMyProfile/UpdateMyProfileScreen.tsx
+++ screens/Profile/UpdateMyProfile/UpdateMyProfileScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react"
-import { View, Text, Image, Pressable, ActivityIndicator } from "react-native"
+import React, { useState } from "react"
+import { View, Text, Pressable, ActivityIndicator, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from "react-native"
 import { styles } from "./styles"
 import { RouteProp, useRoute, useNavigation } from "@react-navigation/native"
 import { User } from "../../../types/User"
@@ -8,10 +8,17 @@ import { updateUserData, updateUserImage, uploadUserImageToStorage } from "../..
 import { peach } from "../../../assets/colors/colors"
 import InputField from "../../../components/InputField/InputField"
 import { showSuccessToast } from "../../../components/ToastMessage/toast"
+import { Ionicons } from "@expo/vector-icons"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { globalStyles } from "../../../assets/global/globalStyles"
+import ProfilePicture from "../../../components/ProfilePicture/ProfilePicture"
+import useKeyboardVisibility from "../../../hooks/useKeyboardVisibility"
+import { EditDescriptionModal } from "../../../components/EditDescriptionModal/EditDescriptionModal"
 
 type RootStackParamList = {
   UpdateProfile: {
       user: User
+      fetchData: () => void
   }
 }
 
@@ -20,13 +27,17 @@ type MapScreenRouteProp = RouteProp<RootStackParamList, 'UpdateProfile'>;
 export const UpdateMyProfileScreen = () => {
   const route = useRoute<MapScreenRouteProp>()
   const navigation = useNavigation()
+  const insets = useSafeAreaInsets()
   const user = route.params.user
+  const fetchData = route.params.fetchData
   const [image, setImage] = useState(user.profilePicture? user.profilePicture : "")
   const [imageLoading, setImageLoading] = useState(false)
-  const [loading, setLoading] = useState(false)
   const [name, setName] = useState(user.firstName)
   const [lastName, setLastName] = useState(user.lastName)
   const [location, setLocation] = useState(user.location)
+  const [description, setDescription] = useState(user.description)
+  const keyboardVisible = useKeyboardVisibility()
+  const [editDescription, setEditDescription] = useState(false)
 
   const pickImage = async () => {
     const result = await launchImageLibraryAsync({
@@ -47,61 +58,81 @@ export const UpdateMyProfileScreen = () => {
     }
   }
 
-  useEffect(() => {
-    setLoading(true)
-    // TODO: Later
-    setLoading(false)
-  }, [])
-
   const submitChanges = async () => {
-    setLoading(true)
     const newData = {
       firstName: name,
       lastName: lastName,
-      location: location
+      location: location,
+      description: description
     }
     const success = await updateUserData(user.uid, newData)
     if(success) {
+      fetchData()
       showSuccessToast("Profile updated successfully!")
       navigation.goBack()
     }
-    setLoading(false)
   }
 
 
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{uri: image}}/>
-      {imageLoading && <ActivityIndicator size="large" color={peach} />}
-      {!imageLoading &&
-        <Pressable onPress={pickImage}>
-          <Text>Update my profile picture</Text>
-        </Pressable>
+    <>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={[styles.main, {paddingTop: insets.top + 5}]}>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => navigation.goBack()} testID="back-button">
+              <Ionicons name="arrow-back-outline" size={24} color={peach} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.container}>
+            <View>
+              <ProfilePicture
+                pictureUrl={image}
+                size={100}
+              />
+              <Pressable testID="update-profile-picture" style={styles.changePictureButton} onPress={pickImage}>
+                <Ionicons name="create" size={26} color={peach} />
+              </Pressable>
+            </View>
+            {imageLoading && <ActivityIndicator size="large" color={peach} />}
+            <View style={styles.inputs}>
+              <InputField
+                label="First Name"
+                value={name}
+                onChangeText={setName}
+              />
+              <InputField
+                label="Last Name"
+                value={lastName}
+                onChangeText={setLastName}
+              />
+              <InputField
+                label="Location"
+                value={location}
+                onChangeText={setLocation}
+              />
+              <InputField
+                label="Description"
+                value={description}
+                editable={false}
+                onPress={() => setEditDescription(true)}
+              />
+            </View>
+            {!keyboardVisible &&
+              <Pressable onPress={submitChanges} style={styles.submitButton}>
+                <Text style={[globalStyles.boldText, styles.buttonText]}>Submit changes</Text>
+              </Pressable>
+            }
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+      {editDescription && 
+        <EditDescriptionModal 
+          editDescription={editDescription}
+          setEditDescription={setEditDescription}
+          description={description}
+          setDescription={setDescription}
+        />
       }
-      {loading &&
-        <Text>soon</Text>
-      }
-      <InputField
-        label="First Name"
-        value={name}
-        onChangeText={setName}
-      />
-      <InputField
-        label="Last Name"
-        value={lastName}
-        onChangeText={setLastName}
-      />
-      <InputField
-        label="Location"
-        value={location}
-        onChangeText={setLocation}
-      />
-      <Pressable onPress={submitChanges}>
-          <Text>Submit changes</Text>
-      </Pressable>
-      <Text style={styles.text1}>soon</Text>
-      <Text style={styles.text2}>soon</Text>
-      <Text style={styles.text3}>soon</Text>
-    </View>
+    </>
   )
 }
