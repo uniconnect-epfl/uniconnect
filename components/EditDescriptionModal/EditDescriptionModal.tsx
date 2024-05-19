ababefdb8aa1958ea0e22c7e2d60e09e5014ffdf
Aidasvenc <79483254+Aidasvenc@users.noreply.github.com>
2024-05-19 18:10:10 +0200

    feat: changed general profile

---
+++ components/EditDescriptionModal/EditDescriptionModal.tsx
@@ -0,0 +1,36 @@
+import { View, Text, Pressable, KeyboardAvoidingView, TouchableWithoutFeedback, TextInput, Platform, Keyboard } from 'react-native'
+import React from 'react'
+import { globalStyles } from '../../assets/global/globalStyles'
+import { styles } from './styles'
+
+interface EditDescriptionModalProps {
+  editDescription: boolean
+  setEditDescription: (value: boolean) => void
+  description: string
+  setDescription: (value: string) => void
+}
+
+export const EditDescriptionModal = ({editDescription, setEditDescription, description, setDescription} : EditDescriptionModalProps) => {
+  return (
+    <>
+      <Pressable style={styles.modalBackground} onPress={() => setEditDescription(!editDescription)}/>
+      <KeyboardAvoidingView style={styles.modal} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <TouchableWithoutFeedback style={styles.modalContainer}  onPress={() => Keyboard.dismiss()}>
+        <View style={styles.modalContainer}>
+          <TextInput
+            style={[styles.description, globalStyles.text]}
+            multiline={true}
+            textAlignVertical="top"
+            onChangeText={setDescription}
+            value={description}
+            placeholder="Enter your description here..."
+          />
+          <Pressable onPress={() => setEditDescription(!editDescription)} style={styles.doneButton}>
+            <Text style={[globalStyles.boldText, styles.buttonText]}>Done</Text>
+          </Pressable>
+        </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </>
+  )
+}
