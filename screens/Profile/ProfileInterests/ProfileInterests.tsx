fc05e32a19037ecc9351dae5a78a2e53bbafbc7f
Fyuryy <91114548+Fyuryy@users.noreply.github.com>
2024-05-23 17:02:38 +0200

    feat: display interests in profile (#235)
    * feat: display interests in profile
    
    * fix: removed duplicated code

--- screens/Profile/ProfileInterests/ProfileInterests.tsx
+++ screens/Profile/ProfileInterests/ProfileInterests.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native"
-import { styles } from "./styles"
 import React from "react"
+import { View, Text, FlatList } from "react-native"
+import { User } from "../../../types/User"
+import styles from "./styles"
+import { globalStyles } from "../../../assets/global/globalStyles"
 
-export const ProfileInterests = () => {
+interface ProfileInterestsProps {
+  user: User
+}
+
+const Interest = ({ interest }: { interest: string }) => (
+  <View style={styles.interestButton}>
+    <Text style={[styles.interestText, globalStyles.text]}>{interest}</Text>
+  </View>
+)
+
+export const ProfileInterests = ({ user }: ProfileInterestsProps) => {
   return (
     <View style={styles.container}>
-      <Text>profile interests</Text>
+      <FlatList
+        data={user.selectedInterests}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <Interest interest={item} />}
+        numColumns={2}
+        style={styles.interestsGrid}
+      />
     </View>
   )
 }
