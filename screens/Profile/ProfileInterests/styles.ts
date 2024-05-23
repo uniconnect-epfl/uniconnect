269ed373759dd9dbd0370be5ba90f3d5fc37f64e
Fyuryy <plagouveia@gmail.com>
2024-05-23 15:23:48 +0200

    fix: removed duplicated code

--- screens/Profile/ProfileInterests/styles.ts
+++ screens/Profile/ProfileInterests/styles.ts
@@ -1,83 +1,11 @@
 import { StyleSheet } from "react-native"
-import {
-  lightGray,
-  peach,
-  black,
-  lightPeach,
-  white,
-  shadowColor,
-} from "../../../assets/colors/colors"
-import { BUTTON_HEIGHT, BUTTON_RADIUS } from "../../../assets/global/constants"
+import { peach, black, white } from "../../../assets/colors/colors"
 
 const styles = StyleSheet.create({
-  buttonSmall: {
-    alignItems: "center",
-    backgroundColor: lightPeach,
-    borderColor: peach,
-    borderRadius: BUTTON_RADIUS,
-    borderWidth: 2,
-    display: "flex",
-    elevation: 8,
-    height: 40,
-    justifyContent: "center",
-    marginHorizontal: 5,
-    shadowColor,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    width: 90,
-  },
-
-  buttonSmallLeft: {
-    backgroundColor: peach,
-  },
-
-  buttonText: {
-    color: black,
-  },
-
-  buttonTextLeft: {
-    color: white,
-  },
-
   container: {
     backgroundColor: white,
     flex: 1,
     padding: 20,
-  },
-
-  footer: {
-    position: "absolute",
-    width: "100%",
-  },
-
-  footerButton: {
-    backgroundColor: white,
-    borderRadius: BUTTON_RADIUS,
-    paddingHorizontal: 30,
-    paddingVertical: 15,
-  },
-  footerButtonText: {
-    color: black,
-    textAlign: "center",
-  },
-
-  image: {
-    alignSelf: "center",
-    height: "10%",
-    marginRight: 10,
-    width: "15%",
-  },
-
-  input: {
-    alignSelf: "center",
-    borderColor: lightGray,
-    borderRadius: BUTTON_RADIUS,
-    borderWidth: 1,
-    height: BUTTON_HEIGHT,
-    marginBottom: 10,
-    paddingHorizontal: 30,
-    width: "95%",
   },
 
   interestButton: {
@@ -102,16 +30,8 @@ const styles = StyleSheet.create({
   },
 
   interestsGrid: {
-      alignSelf: "center",
-      paddingTop: 5,
+    alignSelf: "center",
+    paddingTop: 5,
   },
-
- 
-
-  
-
-  
-
-  
 })
 export default styles
