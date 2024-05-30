1267bbcd4ce73e2fe6d1b4d2c68fe7d9f4da1947
Aidasvenc <79483254+Aidasvenc@users.noreply.github.com>
2024-05-30 09:58:44 +0200

    test: add coverage

--- __test__/components/LowBar/LowBar.test.tsx
+++ __test__/components/LowBar/LowBar.test.tsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react-native'
 import LowBar from '../../../components/LowBar/LowBar'
-
-jest.mock("react", () => ({
-  ...jest.requireActual("react"),
-  useContext: jest.fn(() => ({
-  user: {},
-  firstName: 'firstName',
-  lastName: 'lastName',
-  location: 'location',
-  date: 'date',
-  description: 'description',
-  selectedInterests: ["1", "2"],
-  fromGoogle: false})),
-}))
+import { RegistrationContext } from '../../../contexts/RegistrationContext'
 
 jest.mock("../../../firebase/Registration", () => ({
   storeInitialUserData: jest.fn()
@@ -35,20 +23,122 @@ jest.mock('@react-navigation/native', () => {
 describe('LowBar', () => {
 
     it('navigates to next screen on pressing Next', () => {
-        const { getByText } = render(
+
+      const mockSetSelectedInterests = jest.fn()
+      const mockSelectedInterests = ["one"]
+  
+      const providerProps = {
+        selectedInterests: ["one"],
+        setSelectedInterests: mockSetSelectedInterests,
+        description: mockSelectedInterests,
+        setDescription: jest.fn(),
+        firstName: "first name",
+        setFirstName: jest.fn(),
+        lastName: "last name",
+        setLastName: jest.fn(),
+        date: new Date(),
+        setDate: jest.fn(),
+        location: "",
+        setLocation: jest.fn(),
+        fromGoogle: false
+      }
+      const { getByText } = render(
+          <RegistrationContext.Provider value={providerProps}>
             <LowBar />
+          </RegistrationContext.Provider>
+      )
+      const nextButton = getByText('Next')
+      fireEvent.press(nextButton)
+      expect(mockNavigate).toHaveBeenCalledTimes(1)
+    })
+
+    it('goes back on pressing Back', () => {
+      const mockSetSelectedInterests = jest.fn()
+      const mockSelectedInterests = ["one"]
+  
+      const providerProps = {
+        selectedInterests: ["one"],
+        setSelectedInterests: mockSetSelectedInterests,
+        description: mockSelectedInterests,
+        setDescription: jest.fn(),
+        firstName: "first name",
+        setFirstName: jest.fn(),
+        lastName: "last name",
+        setLastName: jest.fn(),
+        date: new Date(),
+        setDate: jest.fn(),
+        location: "",
+        setLocation: jest.fn(),
+        fromGoogle: false
+      }
+      const { getByText } = render(
+        <RegistrationContext.Provider value={providerProps}>
+          <LowBar />
+        </RegistrationContext.Provider>
+      )
+      const backButton = getByText('Back')
+      fireEvent.press(backButton)
+      expect(mockGoBack).toHaveBeenCalledTimes(1)
+    })
+
+    it("authenticates user after google sign in", () => {
+      const mockSetSelectedInterests = jest.fn()
+      const mockSelectedInterests = ["one"]
+  
+      const providerProps = {
+        user: { email: "email", uid: "uid" },
+        selectedInterests: ["one"],
+        setSelectedInterests: mockSetSelectedInterests,
+        description: mockSelectedInterests,
+        setDescription: jest.fn(),
+        firstName: "first name",
+        setFirstName: jest.fn(),
+        lastName: "last name",
+        setLastName: jest.fn(),
+        date: new Date(),
+        setDate: jest.fn(),
+        location: "",
+        setLocation: jest.fn(),
+        fromGoogle: true,
+        setFromGoogle: jest.fn()
+      }
+        const { getByText } = render(
+          <RegistrationContext.Provider value={providerProps}>
+            <LowBar nextScreen="ExploreTabs" />
+          </RegistrationContext.Provider>
         )
         const nextButton = getByText('Next')
         fireEvent.press(nextButton)
         expect(mockNavigate).toHaveBeenCalledTimes(1)
     })
 
-    it('goes back on pressing Back', () => {
-        const { getByText } = render(
-            <LowBar />
-        )
-        const backButton = getByText('Back')
-        fireEvent.press(backButton)
-        expect(mockGoBack).toHaveBeenCalledTimes(1)
+    it("authenticates after simple sign in", () => {
+      const mockSetSelectedInterests = jest.fn()
+      const mockSelectedInterests = ["one"]
+  
+      const providerProps = {
+        user: null,
+        selectedInterests: ["one"],
+        setSelectedInterests: mockSetSelectedInterests,
+        description: mockSelectedInterests,
+        setDescription: jest.fn(),
+        firstName: "first name",
+        setFirstName: jest.fn(),
+        lastName: "last name",
+        setLastName: jest.fn(),
+        date: new Date(),
+        setDate: jest.fn(),
+        location: "",
+        setLocation: jest.fn(),
+        fromGoogle: false
+      }
+      const { getByText } = render(
+        <RegistrationContext.Provider value={providerProps}>
+          <LowBar nextScreen="ExploreTabs" />
+        </RegistrationContext.Provider>
+      )
+      const nextButton = getByText('Next')
+      fireEvent.press(nextButton)
+      expect(mockNavigate).toHaveBeenCalledTimes(1)
     })
 })
